1b2bcc04ce51e4a379180728c410f3fb811106c8
shirokuma-star <64120061+shirokuma-star@users.noreply.github.com>
2020-11-03 23:36:43 +0900

    Add files via upload

---
+++ Okaimono/HeroBag.java
@@ -0,0 +1,40 @@
+public class HeroBag {
+    private int yakuso = 0;
+    private int shodoku = 0;
+    private int sake = 0;
+    private int enmaku = 0;
+    private int tabako = 0;
+
+    public int getYakuso(){
+        return this.yakuso;
+    }
+    public setYakuso(int yakuso){
+        this.yakuso = yakuso;
+    }
+    public int getShodoku(){
+        return this.shodoku;
+    }
+    public setShodoku(int shodoku){
+        this.shodoku = shodoku;
+    }
+    public int getSake(){
+        return this.sake;
+    }
+    public setSake(int sake){
+        this.sake = sake;
+    }
+    public int getEnmake(){
+        return this.enmaku;
+    }
+    public setEnmaku(int enmaku){
+        this.enmaku = enmaku;
+    }
+    public int getTabako(){
+        return this.tabako;
+    }
+    public setTabako(int tabako){
+        this.tabako = tabako;
+    }
+
+    
+}
---
+++ Okaimono/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    pubalic static void main(String[] args){
+        HeroBag hb = new HeroBag();
+        hb.setYakuso(1);
+        System.out.println(hb.getYakuso);
+    }
+}
